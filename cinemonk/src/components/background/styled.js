bd7b8daee87bc215c41e341e818becc8e3f05aaf
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-12-02 19:18:48 -0300

    opa tinha esquecido padding

--- cinemonk/src/components/background/styled.js
+++ cinemonk/src/components/background/styled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Background = styled.div`
+    padding: 1.5em;
     background-image: url("/assets/images/background.svg");
     background-repeat: no-repeat;
     background-size: 100%;
