d1d04481e114d8a933bf9d1fc8c47b0511bc6d3d
GiovannaRezende <giovannadossantos2004@gmail.com>
2021-12-03 12:33:22 -0300

    página se assentos iniciada

--- cinemonk/src/routes.js
+++ cinemonk/src/routes.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Home from './pages/home';
 import SelectDate from '../src/pages/selectDate/index';
 import SelectHourLanguage from './pages/selectHourLanguage';
+import SelectSeat from './pages/selectSeat';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/" exact={true} component={Home} />
                 <Route path="/date" component={SelectDate} />
                 <Route path="/hour-language" component={SelectHourLanguage} />
+                <Route path="/seat" component={SelectSeat} />
             </Switch>
         </BrowserRouter>
     )
