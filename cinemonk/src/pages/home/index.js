b476598bf71abf3c3a56a73a1e10db3ec190567d
vito <victorybrbr@gmail.com>
2021-12-03 17:16:23 -0300

    home feita

--- cinemonk/src/pages/home/index.js
+++ cinemonk/src/pages/home/index.js
@@ -1,7 +1,20 @@
+import { Background } from '../../components/background/styled';
+import LogoColumn from '../../components/logoColumn';
+import { Container } from './styled'
+import { ButtonStyled } from './button/styled'
+
+import { Link } from 'react-router-dom'
+
 export default function Home() {
     return (
-        <div>
-
-        </div>
+        <Container> 
+            <Background>
+                <div className='conteudo'> 
+                    <LogoColumn />
+                    <div className="titulo"> O que você quer fazer? </div>
+                    <Link to='/date'> <ButtonStyled> Comprar Ingresso </ButtonStyled> </Link>
+                </div> 
+            </Background>
+        </Container>
     )
 }
